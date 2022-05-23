174b5d5efe922eb8ffd56496f960111284147846
lucas <gepeoverload@gmail.com>
2022-05-23 11:27:10 +0200

    implemented way of fetching specific pokemon on form submit

--- dist/main.js
+++ dist/main.js
@@ -3,7 +3,22 @@ var __webpack_exports__ = {};
 /*!********************!*\
   !*** ./src/app.js ***!
   \********************/
+const searchPokemon = document.getElementById("pokemon-search-button");
 
+async function getPokemon(identifier) {
+  const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${identifier}`);
+  const response = await data.json();
+
+  console.log(response);
+}
+
+searchPokemon.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let searchValue = document.getElementById("pokemon-search-input").value;
+  getPokemon(searchValue);
+});
 
 /******/ })()
 ;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBOztBQUVBO0FBQ0EsZ0VBQWdFLFdBQVc7QUFDM0U7O0FBRUE7QUFDQTs7QUFFQTtBQUNBOztBQUVBO0FBQ0E7QUFDQSxDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcG9rZWRleC8uL3NyYy9hcHAuanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3Qgc2VhcmNoUG9rZW1vbiA9IGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKFwicG9rZW1vbi1zZWFyY2gtYnV0dG9uXCIpO1xuXG5hc3luYyBmdW5jdGlvbiBnZXRQb2tlbW9uKGlkZW50aWZpZXIpIHtcbiAgY29uc3QgZGF0YSA9IGF3YWl0IGZldGNoKGBodHRwczovL3Bva2VhcGkuY28vYXBpL3YyL3Bva2Vtb24vJHtpZGVudGlmaWVyfWApO1xuICBjb25zdCByZXNwb25zZSA9IGF3YWl0IGRhdGEuanNvbigpO1xuXG4gIGNvbnNvbGUubG9nKHJlc3BvbnNlKTtcbn1cblxuc2VhcmNoUG9rZW1vbi5hZGRFdmVudExpc3RlbmVyKFwiY2xpY2tcIiwgKGUpID0+IHtcbiAgZS5wcmV2ZW50RGVmYXVsdCgpO1xuXG4gIGxldCBzZWFyY2hWYWx1ZSA9IGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKFwicG9rZW1vbi1zZWFyY2gtaW5wdXRcIikudmFsdWU7XG4gIGdldFBva2Vtb24oc2VhcmNoVmFsdWUpO1xufSk7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
+const searchPokemon = document.getElementById("pokemon-search-button");
 
+async function getPokemon(identifier) {
+  const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${identifier}`);
+  const response = await data.json();
+
+  console.log(response);
+}
+
+searchPokemon.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let searchValue = document.getElementById("pokemon-search-input").value;
+  getPokemon(searchValue);
+});
