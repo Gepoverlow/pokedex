c222716a74cf34ea95be6e2720b272b76504f1b1
lucas <gepeoverload@gmail.com>
2022-05-23 18:46:26 +0200

    added types and colors arrays

--- src/app.js
+++ src/app.js
@@ -1,5 +1,47 @@
 const searchPokemon = document.getElementById("pokemon-search-button");
 
+const types = [
+  "normal",
+  "fire",
+  "water",
+  "grass",
+  "electric",
+  "ice",
+  "fighting",
+  "poison",
+  "ground",
+  "flying",
+  "psyshic",
+  "bug",
+  "rock",
+  "ghost",
+  "dark",
+  "dragon",
+  "steel",
+  "fairy",
+];
+
+const colors = [
+  "#A8A878",
+  "#F08030",
+  "#6890F0",
+  "#78C850",
+  "#F8D030",
+  "#98D8D8",
+  "#C03028",
+  "#A040A0",
+  "#E0C068",
+  "#A890F0",
+  "#F85888",
+  "#A8B820",
+  "#B8A038",
+  "#705898",
+  "#705848",
+  "#7038F8",
+  "#B8B8D0",
+  "#F0B6BC",
+];
+
 async function getPokemon(identifier) {
   const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${identifier}`);
   const response = await data.json();
@@ -14,6 +56,7 @@ function processPokemon(response) {
     name: response.name,
     sprite: response.sprites.front_default,
     moves: handleMoves(response),
+    types: handleTypes(response),
   };
   console.log(tailoredPokemon);
 }
@@ -28,8 +71,24 @@ function handleMoves(response) {
       moves.push(response.moves[i].move.name);
     }
   }
-
   return moves;
+}
+
+function handleTypes(response) {
+  const types = [];
+  for (let i = 0; i < response.types.length; i++) {
+    types.push(response.types[i].type.name);
+  }
+  console.log(types);
+  return types;
+}
+
+function handleBackground(types) {
+  let gradientBg = "";
+
+  if (types.length > 1) {
+    gradientBg = ``;
+  }
 }
 
 function createPokemon() {}
